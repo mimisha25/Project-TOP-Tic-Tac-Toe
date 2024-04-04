cdacfec65d3b81624f91c17ab563935c63bfd459
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-04-04 14:13:04 -0400

    Add function Win. Function will check each combination to determine the winner for both X and O and give the result as a Text

--- index.js
+++ index.js
@@ -43,5 +43,36 @@ function stepZero(target) {
     } else {
         stepCross(target);
     }
+function win() {
+    let comb = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
+    for (let i = 0; i < comb.length; i++) {
+        const cells = document.querySelectorAll(".cells");
+        if (cells[comb[i][0]].textContent === "X" &&
+            cells[comb[i][1]].textContent === "X" &&
+            cells[comb[i][2]].textContent === "X") {
+            res.innerText = 'Winner is X';
+            game.removeEventListener('click', init);
+        }
+
+        else if (cells[comb[i][0]].textContent === "O" &&
+            cells[comb[i][1]].textContent === "O" &&
+            cells[comb[i][2]].textContent === "O") {
+            res.innerText = 'Winner is O';
+            game.removeEventListener('click', init);
+        } else if (count == 9) {
+            res.innerText = `It's a Draw`;
+            game.removeEventListener('click', init);
+        }
+    }
+}
 
