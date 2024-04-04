ffd231f71ddd74e00a0159838a5c2fcc362433e3
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-04-04 14:06:01 -0400

    declared elements from hmtl and add event Listener to button to create grid board 8 cells inside

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+let game = document.querySelector('.game'),
+    res = document.querySelector('.res'),
+    btnGame = document.querySelector('.new-game'),
+    step = false,
+    count = 0
 
+btnGame.addEventListener("click", grid);
+function grid() {
+    for (let i = 0; i <= 8; i++) {
+        const cells = document.createElement("div");
+        cells.classList.add("cells");
+        game.appendChild(cells);
+    }
+
+}
